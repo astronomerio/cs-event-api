3364dbab2e31ecce68987192a3ed6465c35a1acd
willastronomer <will@astronomer.io>
2017-10-06 09:41:14 -0400

    fixed issues with tests

--- pkg/api/v1/route_handler_test.go
+++ pkg/api/v1/route_handler_test.go
@@ -6,13 +6,11 @@ import (
 	"github.com/astronomerio/clickstream-ingestion-api/pkg/ingestion"
 
 	"github.com/gin-gonic/gin"
-	"github.com/hashicorp/consul/logger"
 )
 
 func TestRouteHandler_Register(t *testing.T) {
 	type fields struct {
-		ingestionHandler ingestion.IngestionHandler
-		logger           logger.Logger
+		ingestionHandler ingestion.Handler
 	}
 	type args struct {
 		router *gin.Engine
@@ -28,7 +26,6 @@ func TestRouteHandler_Register(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			h := &RouteHandler{
 				ingestionHandler: tt.fields.ingestionHandler,
-				logger:           tt.fields.logger,
 			}
 			h.Register(tt.args.router)
 		})
