5444ed23981d3933abe0e77aa3cebac3a5a247dc
willastronomer <will@astronomer.io>
2017-10-17 13:34:02 -0400

    remove kinesis

--- pkg/ingestion/ingestion.go
+++ pkg/ingestion/ingestion.go
@@ -2,7 +2,6 @@ package ingestion
 
 import (
 	"github.com/astronomerio/clickstream-ingestion-api/pkg/ingestion/kafka"
-	"github.com/astronomerio/clickstream-ingestion-api/pkg/ingestion/kinesis"
 	"github.com/astronomerio/clickstream-ingestion-api/pkg/logging"
 	"github.com/sirupsen/logrus"
 )
@@ -17,15 +16,6 @@ func NewHandler(kind string) Handler {
 	logger := logging.GetLogger().WithFields(logrus.Fields{"package": "api", "function": "NewHandler"})
 
 	handlers := map[string]func() Handler{
-		"kinesis": func() Handler {
-			return kinesis.NewHandler()
-		},
-		"mock-kinesis": func() Handler {
-			return kinesis.NewMockHandler()
-		},
-		"localstack": func() Handler {
-			return kinesis.NewMockLocalStackHandler()
-		},
 		"kafka": func() Handler {
 			return kafka.NewHandler()
 		},
