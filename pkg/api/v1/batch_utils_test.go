bcb591b9b6b6f90a38bb0e9b21b01adc29e0bb59
Tyler Michael <tyler@astronomer.io>
2017-11-17 11:32:08 -0500

    add mergeFields test

---
+++ pkg/api/v1/batch_utils_test.go
@@ -0,0 +1,18 @@
+package v1
+
+import "testing"
+
+func mergeFields_test(t *testing.T) {
+	dst := map[string]interface{}{
+		"val1": 100,
+		"uniq": "string",
+	}
+	src := map[string]interface{}{
+		"val1": 500,
+		"val2": 100,
+	}
+	mergeFields(&dst, src)
+	if dst["val2"] != 100 {
+		t.Fatal("mergeFields didnt merge as expected")
+	}
+}
