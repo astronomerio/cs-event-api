a5b941cfd8097a1f8801aa68a194b218886c403d
Tyler Michael <tyler@astronomer.io>
2017-11-17 11:33:10 -0500

    add top level fields to the Batch struct

--- pkg/types/v1/batch.go
+++ pkg/types/v1/batch.go
@@ -5,8 +5,10 @@ import (
 )
 
 type Batch struct {
-	SentAt   GenericTime `json:"sentAt,omitempty"`
-	Messages []*Message  `json:"batch,omitempty"`
+	SentAt       GenericTime            `json:"sentAt,omitempty"`
+	Messages     []*Message             `json:"batch,omitempty"`
+	Integrations map[string]interface{} `json:"integrations,omitempty"`
+	Context      map[string]interface{} `json:"traits,omitempty"`
 }
 
 func (b *Batch) String() string {
